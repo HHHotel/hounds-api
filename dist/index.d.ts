659ab6e869adf06bda37869dad9927774a2b1b32
Matt Rochford <mattjohnrochford@gmail.com>
2019-10-27 19:18:23 -0700

    ignore dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/Hounds";
-export * from "./src/HoundsEvents";
-export * from "./src/HoundsUtil";
-export * from "./src/default";
-export * from "./src/types/HHHTypes";
-export * from "./src/types/HHHApiTypes";
--- dist/src/HoundsUtil.d.ts
+++
@@ -1,3 +0,0 @@
-import * as HHH from "./types/HHHTypes";
-export declare function getTimePrepend(event: HHH.IScheduleEvent): string;
-export declare function compareScheduleEvents(a: HHH.IScheduleEvent, b: HHH.IScheduleEvent): number;
