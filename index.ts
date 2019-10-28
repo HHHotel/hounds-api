041089d2b5c388cee633793076e656d0eda9f1cd
Matt Rochford <mattjohnrochford@gmail.com>
2019-10-27 19:08:16 -0700

    npm ignore

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from "./src/Hounds";
 export * from "./src/HoundsEvents";
+export * from "./src/HoundsUtil";
 export * from "./src/default";
 export * from "./src/types/HHHTypes";
 export * from "./src/types/HHHApiTypes";
